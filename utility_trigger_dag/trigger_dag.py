ad42cdd725237728e65dc72dbfadf4952e18b2c5
Raaj Tilak Sarma <68896091+raajpackt@users.noreply.github.com>
2022-08-23 14:37:46 +0530

    fix syntax - quotes

--- utility_trigger_dag/trigger_dag.py
+++ utility_trigger_dag/trigger_dag.py
@@ -11,7 +11,7 @@ def main():
         ),
         json={
             conf: {},
-            dag_run_id: f'{environ.get('AIRFLOW_CTX_DAG_ID')}_{environ.get('AIRFLOW_CTX_DAG_RUN_ID')}'
+            dag_run_id: f'{environ.get("AIRFLOW_CTX_DAG_ID")}_{environ.get("AIRFLOW_CTX_DAG_RUN_ID")}'
         }
     )
     if response.status is 200:
